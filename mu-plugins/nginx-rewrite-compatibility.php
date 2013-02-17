31b98b9da7e088b25a7800cd3f887698966c1d98
Pothi Kalimuthu <pothi@tinywp.com>
2013-02-17 16:23:37 +0530

    Move to mu-plugins folders

--- mu-plugins/nginx-rewrite-compatibility.php
+++ mu-plugins/nginx-rewrite-compatibility.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Plugin Name: Nginx Compatibility
+Plugin Name: Nginx Rewrite Compatibility
 Plugin URI: http://wordpress.org/extend/plugins/nginx-compatibility/
 Version: 0.2.5
 Author: <a href="http://profiles.wordpress.org/vladimir_kolesnikov/">Vladimir Kolesnikov</a>
-Description: A plugin to let WordPress know that mod_rewrite is available.
+Description: A simplified one-line version of Nginx compatibility plugin
 */
 
 # To let WordPress know that the server has mod_rewrite
